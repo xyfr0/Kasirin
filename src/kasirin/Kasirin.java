74b5ddea79782a56b99199ae195bee98d2c2f8a1
dzkyzw <mdzakydzw@gmail.com>
2025-06-03 12:07:48 +0700

    backup

---
+++ src/kasirin/Kasirin.java
@@ -0,0 +1,14 @@
+package kasirin;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kasirin {
+    
+}
